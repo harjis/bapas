7de30251eac35e5f23d4af7a3a5d9abc38105449
Joonas Harjukallio <joonas@devlab.fi>
2015-06-03 16:53:08 +0300

    Added payments relationship

--- bapas_front/app/models/account.js
+++ bapas_front/app/models/account.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  payments: DS.hasMany('payment'),
   name: DS.attr('string'),
   iban: DS.attr('string'),
   created_at: DS.attr('date'),
--- bapas_front/app/models/payment.js
+++ bapas_front/app/models/payment.js
@@ -1,10 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  account: DS.belongsTo('account'),
   entry_date: DS.attr('date'),
   value_date: DS.attr('date'),
   payment_date: DS.attr('date'),
-  amount: DS.attr('float'),
+  amount: DS.attr('string'),
   bic: DS.attr('string'),
   action: DS.attr('string'),
   reference: DS.attr('string'),
