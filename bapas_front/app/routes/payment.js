3f133a4c1786c67ab70b39890659b29333f7759b
Joonas Harjukallio <joonas@devlab.fi>
2015-06-04 16:20:06 +0300

    Beautify

--- bapas_front/app/routes/payment.js
+++ bapas_front/app/routes/payment.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function (params) {
     return this.store.find('payment', params.payment_id);
+  },
+
+  setupController: function (controller, model) {
+    model.reload();
+    controller.set('model', model);
   }
 });
