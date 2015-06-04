7cdd4b0d36bdb87d26575c58c4baf84ce7bee44d
Joonas Harjukallio <joonas@devlab.fi>
2015-06-04 16:09:32 +0300

    Format

--- bapas_front/app/models/payment.js
+++ bapas_front/app/models/payment.js
@@ -1,17 +1,17 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  account:      DS.belongsTo('account', { async: true }),
-  oaccount: DS.belongsTo('oaccount', { async: true }),
-  entryDate:    DS.attr('date'),
-  valueDate:    DS.attr('date'),
-  paymentDate:  DS.attr('date'),
-  amount:       DS.attr('string'),
-  bic:          DS.attr('string'),
-  action:       DS.attr('string'),
-  reference:    DS.attr('string'),
-  message:      DS.attr('string'),
-  cardNumber:   DS.attr('string'),
-  createdAt:    DS.attr('date'),
-  updatedAt:    DS.attr('date')
+  account:     DS.belongsTo('account', { async: true }),
+  oaccount:    DS.belongsTo('oaccount', { async: true }),
+  entryDate:   DS.attr('date'),
+  valueDate:   DS.attr('date'),
+  paymentDate: DS.attr('date'),
+  amount:      DS.attr('string'),
+  bic:         DS.attr('string'),
+  action:      DS.attr('string'),
+  reference:   DS.attr('string'),
+  message:     DS.attr('string'),
+  cardNumber:  DS.attr('string'),
+  createdAt:   DS.attr('date'),
+  updatedAt:   DS.attr('date')
 });
--- bapas_front/app/routes/account.js
+++ bapas_front/app/routes/account.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
   model: function (params) {
     return this.store.find('account', params.account_id);
   },
+
   setupController: function (controller, model) {
     model.reload();
     controller.set('model', model);
