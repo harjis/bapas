ffb0edde4cba536bd820d47ca1281c515e53c372
Joonas Harjukallio <joonas@devlab.fi>
2015-06-04 13:13:29 +0300

    Format

--- bapas_front/app/models/account.js
+++ bapas_front/app/models/account.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  payments: DS.hasMany('payment', {async:true}),
-  name: DS.attr('string'),
-  iban: DS.attr('string'),
+  payments:   DS.hasMany('payment', { async: true }),
+  name:       DS.attr('string'),
+  iban:       DS.attr('string'),
   created_at: DS.attr('date'),
   updated_at: DS.attr('date')
 });
--- bapas_front/app/models/payment.js
+++ bapas_front/app/models/payment.js
@@ -1,16 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  account: DS.belongsTo('account'),
-  entry_date: DS.attr('date'),
-  value_date: DS.attr('date'),
+  account:      DS.belongsTo('account'),
+  entry_date:   DS.attr('date'),
+  value_date:   DS.attr('date'),
   payment_date: DS.attr('date'),
-  amount: DS.attr('string'),
-  bic: DS.attr('string'),
-  action: DS.attr('string'),
-  reference: DS.attr('string'),
-  message: DS.attr('string'),
-  card_number: DS.attr('string'),
-  created_at: DS.attr('date'),
-  updated_at: DS.attr('date')
+  amount:       DS.attr('string'),
+  bic:          DS.attr('string'),
+  action:       DS.attr('string'),
+  reference:    DS.attr('string'),
+  message:      DS.attr('string'),
+  card_number:  DS.attr('string'),
+  created_at:   DS.attr('date'),
+  updated_at:   DS.attr('date')
 });
