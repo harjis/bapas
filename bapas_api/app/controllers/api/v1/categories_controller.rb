6eab501e9ab5cc781897d6dc4a6a17ee4a05d126
Joonas Harjukallio <joonas@devlab.fi>
2015-06-05 11:04:54 +0300

    Add create action for category Remove CSFR-checks

--- bapas_api/app/controllers/api/v1/categories_controller.rb
+++ bapas_api/app/controllers/api/v1/categories_controller.rb
@@ -10,4 +10,20 @@ class Api::V1::CategoriesController < ApplicationController
 
     render 'api/v1/categories/show'
   end
+
+  def create
+    @category = Category.create(category_params)
+
+    if @category.save
+      render 'api/v1/categories/show'
+    else
+      # Figure out what to do
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :parent_id)
+  end
 end
