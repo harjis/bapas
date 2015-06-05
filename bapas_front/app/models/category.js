ec6ae09ebf19225a30511bb99fd260b3775fca41
Joonas Harjukallio <joonas@devlab.fi>
2015-06-05 14:34:34 +0300

    Format

--- bapas_front/app/models/category.js
+++ bapas_front/app/models/category.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   children: DS.hasMany('category', { inverse: 'parent' }),
-  parent:   DS.belongsTo('category', { inverse: 'children' }),
-  name:     DS.attr('string')
+  parent: DS.belongsTo('category', { inverse: 'children' }),
+  name: DS.attr('string')
 });
--- bapas_front/app/models/oaccount.js
+++ bapas_front/app/models/oaccount.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  payments:  DS.hasMany('payment', { async: true, inverse: 'oaccount' }),
-  name:      DS.attr('string'),
-  iban:      DS.attr('string'),
+  payments: DS.hasMany('payment', { async: true, inverse: 'oaccount' }),
+  name: DS.attr('string'),
+  iban: DS.attr('string'),
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date')
 });
