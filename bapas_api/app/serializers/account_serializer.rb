b6b6a4da6e48c32cddeda28b35fac19eb92f0483
Joonas Harjukallio <joonas@devlab.fi>
2015-06-05 08:35:56 +0300

    Remove Grape

--- bapas_api/app/serializers/account_serializer.rb
+++
@@ -1,3 +0,0 @@
-class AccountSerializer < ActiveModel::Serializer
-  attributes :id, :name, :iban, :created_at, :updated_at
-end
--- bapas_api/config/routes.rb
+++ bapas_api/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  mount API::Base, at: "/grape_api"
 
   namespace :api, defaults: { format: :json }, format: false do
     namespace :v1 do
