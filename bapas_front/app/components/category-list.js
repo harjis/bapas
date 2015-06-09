5d05bfaf871439a1b8541406ed63c38d67e87644
Joonas Harjukallio <joonas@devlab.fi>
2015-06-09 10:47:43 +0300

    Recursive CategoryListComponent

--- bapas_front/app/components/category-list.js
+++ bapas_front/app/components/category-list.js
@@ -3,12 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   filterBy: 'roots',
   filters: {
+    all: function () {
+      return true;
+    },
     roots: function (category) {
       return category.get('depth') === 0;
     }
   },
   filteredList: function () {
-    var filterFunction = this.filters[this.get('filterBy')];
+    var filterFunction = null;
+    if (this.get('root')) {
+      filterFunction = this.filters['roots'];
+    }
+    else {
+      filterFunction = this.filters['all'];
+    }
     return this.get('list').filter(filterFunction);
   }.property('list', 'filterBy'),
   actions: {
