93446ec2bba4d4698ce0d5bce5040507598ee904
Joonas Harjukallio <joonas@devlab.fi>
2015-06-09 10:28:28 +0300

    Working filter in CategoryListComponent

--- bapas_front/app/components/category-list.js
+++ bapas_front/app/components/category-list.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  filterBy: 'roots',
+  filters: {
+    roots: function (category) {
+      return category.get('depth') === 0;
+    }
+  },
+  filteredList: function () {
+    var filterFunction = this.filters[this.get('filterBy')];
+    return this.get('list').categories.filter(filterFunction);
+  }.property('list', 'filterBy'),
   actions: {
     selectCategory: function (category) {
       this.sendAction('selectedCategory', category);
