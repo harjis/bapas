e1311c12e441b1a46dbb035d548317ef495eaa5a
Joonas Harjukallio <joonas@devlab.fi>
2015-06-10 21:21:06 +0300

    Pre select correct value

--- bapas_front/app/controllers/oaccount.js
+++ bapas_front/app/controllers/oaccount.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  selectedCategory: null,
   actions: {
     saveOaccount: function () {
       var category_id = this.get('selectedCategory');
