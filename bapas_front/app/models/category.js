3046fa09a9a9eed1d9c317d228d45c47bc7ee671
Joonas Harjukallio <joonas@devlab.fi>
2015-06-10 16:57:03 +0300

    Add Category - OtherAccount relationship to front-end

--- bapas_front/app/models/category.js
+++ bapas_front/app/models/category.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   children: DS.hasMany('category', { inverse: 'parent' }),
   parent: DS.belongsTo('category', { inverse: 'children' }),
+  oaccounts: DS.hasMany('oaccount'),
   name: DS.attr('string'),
   children_count: DS.attr('number'),
   depth: DS.attr('number'),
