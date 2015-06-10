d479440cbcec104650b38510e32a4f26c220409a
Joonas Harjukallio <joonas@devlab.fi>
2015-06-10 19:59:01 +0300

    Add update action for OtherAccount

--- bapas_api/app/controllers/api/v1/other_accounts_controller.rb
+++ bapas_api/app/controllers/api/v1/other_accounts_controller.rb
@@ -10,4 +10,20 @@ class Api::V1::OtherAccountsController < ApplicationController
 
     render 'api/v1/other_accounts/show'
   end
+
+  def update
+    @account = OtherAccount.find(params[:id])
+    @account.category_id = params[:oaccount][:category]
+    if @account.save
+      render status: 200, json: {}
+    else
+      render status: 500, json: {}
+    end
+  end
+
+  private
+
+  def other_account_params
+    params.require(:oaccount).permit(:category)
+  end
 end
---
+++ bapas_front/app/controllers/oaccount.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  selectedCategory: null,
+  actions: {
+    saveOaccount: function () {
+      var category_id = this.get('selectedCategory');
+      var category = this.store.find('category', category_id);
+      this.store.find('oaccount', 2).then(function (oaccount) {
+        oaccount.set('category', category);
+        oaccount.save();
+      });
+    }
+  }
+});
