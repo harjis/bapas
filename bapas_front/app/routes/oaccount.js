c37e1c7cd196a4949fca23dde203b9674d38b2aa
Joonas Harjukallio <joonas@devlab.fi>
2015-06-15 12:54:17 +0300

    Maybe need to do this?

--- bapas_front/app/routes/oaccount.js
+++ bapas_front/app/routes/oaccount.js
@@ -4,5 +4,6 @@ export default Ember.Route.extend({
   setupController: function (controller, model) {
     controller.set('model', model);
     controller.set('categories', this.store.find('category'));
+    controller.set('selectedCategory', model.get('category'));
   }
 });
