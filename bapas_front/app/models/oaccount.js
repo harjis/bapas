924d33ef503a28cf6acfbe404a9ec750afa74a5e
Joonas Harjukallio <joonas@devlab.fi>
2015-07-07 16:35:56 +0300

    Format

--- bapas_front/app/models/oaccount.js
+++ bapas_front/app/models/oaccount.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  payments: DS.hasMany('payment', {inverse: 'oaccount' }),
+  payments: DS.hasMany('payment', { inverse: 'oaccount' }),
   category: DS.belongsTo('category'),
   name: DS.attr('string'),
   iban: DS.attr('string'),
