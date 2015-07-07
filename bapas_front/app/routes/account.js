8359b92712a79db42b90229d2c0d715f609933a8
Joonas Harjukallio <joonas@devlab.fi>
2015-07-07 17:07:23 +0300

    Hackety hack! Routes model hook is not accessed when link-to is pressed. Thats why we need to give the account.id to the link-to helper so that model hook gets called

--- bapas_front/app/routes/account.js
+++ bapas_front/app/routes/account.js
@@ -2,21 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
+    var categories = this.store.find('category');
+    var account = this.store.find('account', params.account_id);
+
     return Ember.RSVP.hash({
-      account: this.store.find('account', params.account_id),
-      categories: this.store.find('category')
+      account: account,
+      categories: categories
     });
   },
 
   setupController: function (controller, model) {
-    model.account.reload();
-    controller.set('model', model.account);
+    controller.setProperties(model);
 
     var categoryLabels = model.categories.map(function (c) {
       return c.get('name');
     });
 
     controller.set('categoryLabels', categoryLabels);
-    controller.set('categoryData', [0,1,2]);
+    controller.set('categoryData', [0, 1, 2]);
   }
 });
