2cef18b29504638e1c265965289dd173b61a2fa2
Joonas Harjukallio <joonas@devlab.fi>
2015-07-08 10:13:48 +0300

    Move chart data grouping to controller & refactor

--- bapas_front/app/controllers/account.js
+++ bapas_front/app/controllers/account.js
@@ -10,9 +10,43 @@ export default Ember.Controller.extend({
       this.set('date', date);
     }
   },
+  categoryLabels: function () {
+    return this.get('categories').map(function (category) {
+      return category.get('name');
+    });
+  },
+  categoryIds: function () {
+    return this.get('categories').map(function (category) {
+      return category.get('id');
+    });
+  },
+  categoryData: function (ids) {
+    var categoryData = {};
+    for (var i in ids) {
+      var categoryId = ids[i];
+      categoryData[categoryId] = 0;
+      this.get('account').get('payments').forEach(function (payment) {
+        var currentCategoryId = payment.get('oaccount').get('category').get('id');
+        var amount = Math.abs(parseFloat(payment.get('amount')));
+
+        if (currentCategoryId === categoryId) {
+          categoryData[categoryId] += amount;
+        }
+      });
+    }
+
+    var categoryIds = Object.keys(categoryData);
+    return categoryIds.map(function (categoryId) {
+      return categoryData[categoryId];
+    });
+  },
   data: function () {
+    var categoryIds = this.categoryIds();
+    var categoryLabels = this.categoryLabels();
+    var categoryData = this.categoryData(categoryIds);
+
     return {
-      labels: this.get('categoryLabels'),
+      labels: categoryLabels,
       datasets: [
         {
           fillColor: "rgba(151,187,205,0.2)",
@@ -21,7 +55,7 @@ export default Ember.Controller.extend({
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
           pointHighlightStroke: "rgba(151,187,205,1)",
-          data: this.get('categoryData')
+          data: categoryData
         }
       ]
     };
--- bapas_front/app/routes/account.js
+++ bapas_front/app/routes/account.js
@@ -15,32 +15,5 @@ export default Ember.Route.extend({
 
   setupController: function (controller, model) {
     controller.setProperties(model);
-
-    var categoryIds = [];
-    var categoryLabels = model.categories.map(function (c) {
-      categoryIds.push(c.get('id'));
-      return c.get('name');
-    });
-
-    var categoryData = {};
-    $.each(categoryIds, function (_, categoryId) {
-      categoryData[categoryId] = 0;
-      model.account.get('payments').forEach(function (payment) {
-        var currentCategoryId = payment.get('oaccount').get('category').get('id');
-        var amount = Math.abs(parseFloat(payment.get('amount')));
-
-        if (currentCategoryId === categoryId) {
-          categoryData[categoryId] += amount;
-        }
-      });
-    });
-
-    var keys = Object.keys(categoryData);
-    categoryData = keys.map(function (v) {
-      return categoryData[v];
-    });
-
-    controller.set('categoryLabels', categoryLabels);
-    controller.set('categoryData', categoryData);
   }
 });
