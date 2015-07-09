afca4f6702e096bb9110d37b162e22a18ca3ad77
Joonas Harjukallio <harjukallio@gmail.com>
2015-07-09 16:07:47 +0300

    Oaccount table with category select

--- bapas_front/app/routes/oaccounts/index.js
+++ bapas_front/app/routes/oaccounts/index.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return this.store.find('oaccount');
+    var oaccounts = this.store.find('oaccount');
+    var categories = this.store.find('category');
+
+    return Ember.RSVP.hash({
+      categories: categories,
+      oaccounts: oaccounts,
+    });
+  },
+
+  setupController: function (controller, model) {
+    controller.setProperties(model);
   }
 });
