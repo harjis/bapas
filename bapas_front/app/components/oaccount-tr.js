6106f63c3d33f9a02b96815da30acc455771f174
Joonas Harjukallio <harjukallio@gmail.com>
2015-07-10 14:21:53 +0300

    Oaccount table component changed to tr component Implement oaccount save from index

--- bapas_front/app/components/oaccount-tr.js
+++ bapas_front/app/components/oaccount-tr.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'tr',
+  saveOaccount: "saveOaccount",
   actions: {
     saveOaccount: function (oaccount) {
-      this.sendAction('saveOaccount', oaccount);
+      this.sendAction('saveOaccount', oaccount, this.get('selectedCategory'));
     }
   }
 });
