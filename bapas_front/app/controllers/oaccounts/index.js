8629681c90d0c76dfe3a78824e2cc0363a1b0f34
Joonas Harjukallio <harjukallio@gmail.com>
2015-07-17 16:04:43 +0300

    Back to basics

--- bapas_front/app/controllers/oaccounts/index.js
+++ bapas_front/app/controllers/oaccounts/index.js
@@ -1,41 +1,14 @@
 import Ember from 'ember';
-import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Controller.extend({
-  queryParams: ['page', 'perPage', 'filter'],
+  queryParams: ['page', 'perPage'],
+
+  pageBinding: 'oaccounts.page',
+  perPageBinding: 'oaccounts.perPage',
+  totalPagesBinding: 'oaccounts.totalPages',
 
   page: 1,
   perPage: 10,
-  filter: '',
-
-  pagedContent: pagedArray("filteredContent", {
-    pageBinding: "page",
-    perPageBinding: "perPage",
-    totalPagesBinding: 'model.totalPages',
-  }),
-
-  //TODO
-  // I do not get this. Why do I have to return Ember.ArrayProxy. And why routes model has to have content key in it.
-  // And why this has to be arrangedContent?
-  arrangedContent: function () {
-    return Ember.ArrayProxy.create({
-      content: this.get('model')
-    });
-  }.property('model'),
-
-  filteredContent: function () {
-    var filter = this.get('filter');
-    var oaccounts = this.get('arrangedContent');
-
-    if (filter && filter === 'categoryless') {
-      return oaccounts.filter(function (oaccount) {
-        return !oaccount.get('hasCategory');
-      });
-    }
-    else {
-      return oaccounts;
-    }
-  }.property('arrangedContent.@each.hasCategory', 'filter'),
 
   actions: {
     saveOaccount: function (oaccount, selectedCategory) {
--- bapas_front/app/routes/oaccounts/index.js
+++ bapas_front/app/routes/oaccounts/index.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
 export default Ember.Route.extend(RouteMixin, {
+  queryParams: {
+    page: {
+      refreshModel: true
+    }
+  },
   model: function (params) {
     var oaccounts = this.findPaged('oaccount', params);
     var categories = this.store.find('category');
@@ -9,7 +14,7 @@ export default Ember.Route.extend(RouteMixin, {
     //TODO why do we have to reutnr oaccounts under content key?
     return Ember.RSVP.hash({
       categories: categories,
-      content: oaccounts,
+      oaccounts: oaccounts,
     });
   },
 
