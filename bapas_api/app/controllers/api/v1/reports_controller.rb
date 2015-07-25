2869c9a33e9dcb75a7e54c2a9ff9a76cc4b7b261
Joonas Harjukallio <harjukallio@gmail.com>
2015-07-25 11:43:30 +0300

    Separate positive and negative values

--- bapas_api/app/controllers/api/v1/reports_controller.rb
+++ bapas_api/app/controllers/api/v1/reports_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::ReportsController < ApplicationController
   def show
     @date = Date.parse params[:date]
-    @payments = Payment.joins(:other_account).where(account_id: 1)
-    @payments = @payments.where('extract(year from entry_date) = ?', @date.year)
-    @payments = @payments.where('extract(month from entry_date) = ?', @date.month)
-    @payments = @payments.select('other_accounts.category_id, SUM(payments.amount) AS sum_amount')
-    @payments_by_category = @payments.group('other_accounts.category_id')
+    @negative_payments = Payment.joins(:other_account).where(account_id: 1)
+    @negative_payments = @negative_payments.where('amount < 0')
+    @negative_payments = @negative_payments.where('extract(year from entry_date) = ?', @date.year)
+    @negative_payments = @negative_payments.where('extract(month from entry_date) = ?', @date.month)
+    @negative_payments = @negative_payments.select('other_accounts.category_id, SUM(payments.amount) AS sum_amount')
+    @negative_payments = @negative_payments.group('other_accounts.category_id')
+
+    @positive_payments = Payment.joins(:other_account).where(account_id: 1)
+    @positive_payments = @positive_payments.where('amount >= 0')
+    @positive_payments = @positive_payments.where('extract(year from entry_date) = ?', @date.year)
+    @positive_payments = @positive_payments.where('extract(month from entry_date) = ?', @date.month)
+    @positive_payments = @positive_payments.select('other_accounts.category_id, SUM(payments.amount) AS sum_amount')
+    @positive_payments = @positive_payments.group('other_accounts.category_id')
     @categories = Category.all
 
     render 'api/v1/reports/show'
--- bapas_api/app/views/api/v1/reports/show.json.jbuilder
+++ bapas_api/app/views/api/v1/reports/show.json.jbuilder
@@ -1,7 +1,20 @@
 json.report do
   json.id @date.strftime('%Y-%m-%d')
-  json.report_data do
-    json.array! @payments_by_category do |payment_sum|
+  json.negative_payments do
+    json.array! @negative_payments do |payment_sum|
+      if payment_sum.category_id
+        json.category_id payment_sum.category_id
+        json.category_name @categories.find(payment_sum.category_id).name
+      else
+        json.category_id 0
+        json.category_name '*not categorized*'
+      end
+
+      json.amount payment_sum.sum_amount
+    end
+  end
+  json.positive_payments do
+    json.array! @positive_payments do |payment_sum|
       if payment_sum.category_id
         json.category_id payment_sum.category_id
         json.category_name @categories.find(payment_sum.category_id).name
