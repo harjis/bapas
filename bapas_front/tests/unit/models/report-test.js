2a9c368036fe8ebe0c89858394e19d689cc8e191
Joonas Harjukallio <harjukallio@gmail.com>
2015-07-27 13:26:19 +0300

    Fix Report test

--- bapas_front/tests/unit/models/report-test.js
+++ bapas_front/tests/unit/models/report-test.js
@@ -1,21 +1,17 @@
 import { moduleForModel, test } from 'ember-qunit';
-import Report from "bapas_front/models/report";
 
 moduleForModel('report', 'Unit | Model | report', {
   needs: []
 });
 
-test('it exists', function(assert) {
+test('it exists', function (assert) {
   var model = this.subject();
-  // var store = this.store();
   assert.ok(!!model);
 });
 
-test('returns moment date', function(assert) {
-  var report = Report.create({});
-
-  report.set('id', '2015-01-01');
+test('returns moment date', function (assert) {
+  var report = this.subject({ id: '2015-01-01' });
 
   assert.equal(report.get('date')._isAMomentObject, true);
-  assert.equal(report.get('date').format('DD-MM-YYY'), '01-01-2015');
+  assert.equal(report.get('date').format('DD-MM-YYYY'), '01-01-2015');
 });
